dfc9b9e106f7b92ab076ad90656b8011482c7df0
Jon Wheeler <jonathanwheeler8@gmail.com>
2017-01-19 13:52:53 -0700

    Assignment1 initial commit

---
+++ Assignment1/DivisibleBy7/DivisibleBy7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisibleBy7
+{
+    class DivisibleBy7
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter a number.");
+                return 1;
+            }
+
+            int num;
+            bool test = int.TryParse(args[0], out num);
+            if (test == false)
+            {
+                System.Console.WriteLine("Please enter a numeric argument.");
+                System.Console.WriteLine("Usage: Factorial <num>");
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
