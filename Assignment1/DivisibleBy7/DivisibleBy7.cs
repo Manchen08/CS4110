201f4576604c724576480c96b5e6b91070369a98
Jon <jonathanwheeler8@gmail.com>
2017-01-19 14:33:21 -0700

    Finished DivisibleBy7, started PowersOf2

--- Assignment1/DivisibleBy7/DivisibleBy7.cs
+++ Assignment1/DivisibleBy7/DivisibleBy7.cs
@@ -20,11 +20,23 @@ namespace DivisibleBy7
             bool test = int.TryParse(args[0], out num);
             if (test == false)
             {
-                System.Console.WriteLine("Please enter a numeric argument.");
-                System.Console.WriteLine("Usage: Factorial <num>");
+
+                System.Console.WriteLine("Please enter a number.");
                 return 1;
             }
 
+            int a1 = 7;
+
+            if (num % a1 == 0)
+            {
+                System.Console.WriteLine("X = {0}  Member?  True", num);
+            }
+            else
+            {
+                System.Console.WriteLine("X = {0}  Member?  False", num);
+            }
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
             return 0;
         }
     }
---
+++ Assignment1/PowersOf2/PowersOf2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowersOf2
+{
+    class PowersOf2
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
